65c61a02aa1923b0a57d86db32aa54dcb415acd1
jacobortiz <54605823+jacobortiz@users.noreply.github.com>
2020-09-01 21:21:01 -0700

    working on login activity

---
+++ app/src/main/java/com/example/projectonecourseapp/LoginActivity.java
@@ -0,0 +1,30 @@
+package com.example.projectonecourseapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        final EditText username = findViewById(R.id.username);
+        final EditText password = findViewById(R.id.password);
+
+        Button login_button = findViewById(R.id.login_button);
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username_ = username.getText().toString();
+                String password_ = password.getText().toString();
+
+            }
+        });
+    }
+}
